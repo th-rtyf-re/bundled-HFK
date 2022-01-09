b76a9c18b1297329914ffaaf239c762461e613f0
Isaac Ren <isaac.ren@inria.fr>
2022-01-09 11:34:52 +0100

    Added and updated GNU Licence

--- example/full_interface/Full_interface.cpp
+++ example/full_interface/Full_interface.cpp
@@ -1,5 +1,27 @@
-#define BUNDLED_HFK_DRAW_  // define for LaTeX-related functionality
-//#define BUNDLED_HFK_VERBOSE_  // define for more verbose console
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
+ *                                                                           *
+ *  Bundled HFK - a knot Floer homology calculator                           *
+ *                                                                           *
+ *  Copyright (C) 2021-2022  Isaac Ren                                       *
+ *  For further details, contact Isaac Ren (gopi3.1415@gmail.com).           *
+ *                                                                           *
+ *  This program is free software: you can redistribute it and/or modify     *
+ *  it under the terms of the GNU General Public License as published by     *
+ *  the Free Software Foundation, either version 3 of the License, or        *
+ *  (at your option) any later version.                                      *
+ *                                                                           *
+ *  This program is distributed in the hope that it will be useful,          *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
+ *  GNU General Public License for more details.                             *
+ *                                                                           *
+ *  You should have received a copy of the GNU General Public License        *
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.   *
+ *                                                                           *
+\* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+//#define BUNDLED_HFK_DRAW_  // define for LaTeX-related functionality
+#define BUNDLED_HFK_VERBOSE_  // define for more verbose console
 
 #include <ctime>
 #include <iostream>
@@ -9,12 +31,14 @@
 
 #include "Knot_interface.h"
 
-void aux(std::ifstream& knots_csv,
-         std::ofstream& knot_diagram_out,
-         std::ofstream& planar_diagram_out,
-         std::ofstream& polynomial_out,
-         int start = 1,
-         int stop = -1) {
+void load_and_compute(
+  std::ifstream& knots_csv,
+  std::ofstream& knot_diagram_out,
+  std::ofstream& planar_diagram_out,
+  std::ofstream& polynomial_out,
+  int start = 1,
+  int stop = -1
+) {
   while (--start) {
     --stop;
     knots_csv.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
@@ -45,14 +69,62 @@ void aux(std::ifstream& knots_csv,
   }
 }
 
-int main(int argc, char* argv[]) {
-  if (argc <= 2) {
+void print_help() {
+  std::cout
+    << "Bundled HFK options:\n"
+    << "  --gnu [w] [c]\n"
+    << "  -h, --help\n"
+    << "  -me, --morse-events <csv file>\n"
+    << "  -pd, --planar-diagram <txt file>\n"
+    << "  -rs, --regina-signature <csv file> [<knot number/start>] [<knot end>]\n";
+}
+
+
+void print_license(std::string option = "") {
+  if (option == "w") {
     std::cout
-      << "arguments: "
-      << "-rs/--regina-signature [csv file] [knot number/start] [knot end], "
-      << "-pd/--planar-diagram [txt file], "
-      << "-me/--morse-events [csv file]."
-      << std::endl;
+      << "This program is distributed in the hope that it will be useful, "
+      << "but WITHOUT ANY WARRANTY; without even the implied warranty of "
+      << "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the "
+      << "GNU General Public License for more details.\n";
+  }
+  else if (option == "c") {
+    std::cout
+      << "This program is free software: you can redistribute it and/or modify "
+      << "it under the terms of the GNU General Public License as published by "
+      << "the Free Software Foundation, either version 3 of the License, or "
+      << "(at your option) any later version.\n";
+  }
+}
+
+
+int main(int argc, char* argv[]) {
+  std::cout
+    << "\nBundled HFK - a bordered Floer knot homology calculator\n\n"
+    << "Copyright (C) 2021-2022  Isaac Ren\n\n"
+    << "This program comes with ABSOLUTELY NO WARRANTY; for details run the "
+    << "program with option `-gnu w'.\n"
+    << "This is free software, and you are welcome to redistribute it under "
+    << "certain conditions; run the program with option `-gnu c' for details.\n"
+    << std::endl;
+  
+  if (argc == 1 or std::string(argv[1]) == "-h" or std::string(argv[1]) == "--help") {
+    print_help();
+    return 0;
+  }
+  
+  if (std::string(argv[1]) == "-gnu") {
+    if (argc == 2) {
+      print_license();
+    }
+    else {
+      print_license(std::string(argv[2]));
+    }
+    return 0;
+  }
+  
+  if (argc == 2) {
+    print_help();
     return 0;
   }
   
@@ -115,13 +187,13 @@ int main(int argc, char* argv[]) {
         << std::endl;
       
       in_file.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
-      aux(in_file, knot_diagram_out, planar_diagram_out, polynomial_out, start, stop);
+      load_and_compute(in_file, knot_diagram_out, planar_diagram_out, polynomial_out, start, stop);
     }
     else {
       std::cout << "[main] Reading Regina signatures from " << argv[2] << "..." << std::endl;
       /* get rid of first line */
       in_file.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
-      aux(in_file, knot_diagram_out, planar_diagram_out, polynomial_out);
+      load_and_compute(in_file, knot_diagram_out, planar_diagram_out, polynomial_out);
     }
     
     knot_diagram_out.close();
